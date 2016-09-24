f4df3ebcefb11225f079ed67de72566ac4fdcfa2
Vodzo <zelic@live.com>
2016-09-25 00:26:44 +0200

    reorganising

--- chatco.js
+++ chatco.js
@@ -1,2 +1,9 @@
-require('./lib/server.js');
-//require('./lib/bot.js');
+var bot = require('./lib/bot.js')
+var server = require('./lib/server.js');
+
+server.app.post('/message', function(req, res) {
+  bot.bot.api.channels.create({name:"test"}, function(err, res) {
+    console.log(res);
+  });
+});
+
