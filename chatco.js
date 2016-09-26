c6ed1f92c40f0a7230bfc906c2e8ed2b0cb5d3d7
Vodzo <zelic@live.com>
2016-09-26 23:52:27 +0200

    Add Slack App

--- chatco.js
+++ chatco.js
@@ -3,7 +3,20 @@ var server = require('./lib/server.js');
 
 server.app.post('/message', function(req, res) {
   bot.bot.api.channels.create({name:"test"}, function(err, res) {
-    console.log(res);
+    
   });
 });
+
+bot.controller.configureSlackApp({
+  port:3000,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  scopes: ['incoming-webhook','team:read','users:read','channels:write','im:read','im:write','groups:read','emoji:read','chat:write:bot']
+});
+
+// set up web endpoints for oauth, receiving webhooks, etc.
+bot.controller.createOauthEndpoints(server.app);
+
+
+
 
